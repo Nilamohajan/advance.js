c1f61946aa08f13514a17f23cfb708b16f096f47
Nilamohajan <nila1997ctg@gmail.com>
2020-03-21 22:50:41 +0600

    true-flase

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//Falsy:
+//false
+// 0
+// ""
+//undefined
+// null
+// NaN
+//Truthy
+//'0', ' ' []
+
+
+//const name = "0";
+let name = 10;
+console.log(name);
+if(name  || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

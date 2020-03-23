58b6fa0aa08cb8a8929eabdf0fe638691b1eb636
Nilamohajan <nila1997ctg@gmail.com>
2020-03-23 20:51:39 +0600

    null-vs-undefined

---
+++ nul-vs-undefined.js
@@ -0,0 +1,19 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+ function add(num1, num2=10){
+
+     console.log(num1, num2);
+}
+
+const pran = {name:"smart dude", phone:23865};
+console.log(pran.);
+
+let fun = undefined;
+console.log(fun);
+
+
+null ages = [2, 3,5]
+console.log(ages[11]);
